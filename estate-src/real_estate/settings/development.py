b7553af5c469f5f221a7093dbc8be275addf6760
shivamurkude <shivamurkude2000@gmail.com>
2023-06-09 18:23:16 +0530

    splitting settings.py

---
+++ estate-src/real_estate/settings/development.py
@@ -0,0 +1,26 @@
+from .base import *
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST=env("EMAIL_HOST")
+EMAIL_USE_TLS=True
+EMAIL_PORT=env("EMAIL_PORT")
+EMAIL_HOST_USER=env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD=env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL="info@real-estate.com"
+DOMAIN=env("DOMAIN")
+SITE_NAME="Real Estate"
+
+
+
+
+DATABASES = {
+   
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env("DJANGO_POSTGRES_DATABASE"),
+        'USER': env("DJANGO_POSTGRES_USER"),
+        'PASSWORD': env("DJANGO_POSTGRES_PASSWORD"),
+        'HOST': env("DJANGO_POSTGRES_HOST"),
+        'PORT': env("DJANGO_POSTGRES_PORT"),
+    },
+}
--- estate-src/real_estate/urls.py
+++ estate-src/real_estate/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+    path("supersecret/", admin.site.urls),
+]+  static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT)
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to the Real Estate Portal"
