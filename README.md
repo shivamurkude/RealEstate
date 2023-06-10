2bc7e02b61774986b1747c634b429c644f16a6d7
shivamurkude <shivamurkude2000@gmail.com>
2023-06-10 23:55:59 +0530

    whole basic file setup

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# RealEstate
-Django Docker Ngnix 
+# Estate code 
+
+
+## Database Setup Commands:
+```bash
+CREATE DATABASE estate;
+CREATE USER admin WITH PASSWORD 'admin';
+ALTER ROLE admin SET client_encoding TO 'utf8';
+ALTER ROLE admin SET default_transaction_isolation TO 'read committed';
+ALTER ROLE admin SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE estate TO admin;
+```
+
+
---
+++ real_estate/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+     path("supersecret/", admin.site.urls),
+     path("api/v1/auth/",include("djoser.urls")),
+     path("api/v1/auth/",include("djoser.urls.jwt")),
+     path("api/v1/profile/",include("apps.profiles.urls")),
+]+  static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT)
+
+
+admin.site.site_header ="Real Estate Admin"
+admin.site.site_title="Real estate Admin Portal"
+admin.site.index_title="Welcome to the Real estate Portal"
